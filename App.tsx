b2d755a29325c1e33e1dc85dc0b000c638d9d78a
shin donggyu <donggyu9410@gmail.com>
2020-09-13 02:55:03 +0900

    Start Project

---
+++ App.tsx
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+} from 'react-native';
+
+import React from 'react';
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Link SDK</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
